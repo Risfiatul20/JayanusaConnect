75fd0c549896c4de03fb46723ed25b0ee3d89799
Risfiatul20 <risfiatul@example.com>
2026-05-22 01:28:06 +0700

    fix: Add is_verified to User fillable and casts - alumni now correctly starts as unverified

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,23 +11,18 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-#[Fillable(['name', 'nim', 'email', 'password', 'role', 'photo', 'phone', 'address', 'angkatan', 'prodi'])]
+#[Fillable(['name', 'nim', 'email', 'password', 'role', 'photo', 'phone', 'address', 'angkatan', 'prodi', 'is_verified'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable, HasApiTokens;
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'password'          => 'hashed',
+            'is_verified'       => 'boolean',
         ];
     }
 
